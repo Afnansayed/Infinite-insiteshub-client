f0266a751d4cb650492504d1ab29ff7f40d212a1
Afnansayed <afnansayed1973@gmail.com>
2024-05-09 21:51:25 +0600

    login and register page ui design

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -1,10 +1,35 @@
+import { Link } from "react-router-dom";
 
 
 const LogIn = () => {
     return (
-        <div>
-            <h2 className="text-center text-3xl">This is logIn page</h2>
+        <div className="hero min-h-screen bg-sky-100">
+        <div className=" w-[70%]">
+            <div className=" card shrink-0  shadow-2xl bg-sky-500 ">
+                <form className="card-body">
+                    
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" className="input input-bordered" name="email" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" name="password" required />
+                    </div>
+                    <div className="form-control mt-6 lg:col-span-2">
+                        <button className="btn bg-blue-100 hover:bg-red-300 text-blue-900 font-bold ">Login</button>
+                    </div>
+                </form>
+                {/* <p className="text-green-600 text-center mb-5"></p>
+                <p className="text-red-600 text-center mb-5"></p> */}
+                <p className="pb-5 text-center">If you do not have an account please <Link to='/login' className="text-[#fff]">Register</Link></p>
+            </div>
         </div>
+    </div>
     );
 };
 
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,46 @@
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
     return (
-        <div>
-            <h2 className="text-center text-3xl">This is logIn page</h2> 
+        <div className="hero min-h-screen bg-sky-100">
+        <div className=" w-[70%]">
+            <div className=" card shrink-0  shadow-2xl bg-sky-500 ">
+                <form className="card-body grid grid-cols-1 lg:grid-cols-2">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" placeholder="Enter Your Name" className="input input-bordered" name="name" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" className="input input-bordered" name="email" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" name="password" required />
+                    </div>
+                    <div className="form-control lg:col-span-2">
+                        <label className="label">
+                            <span className="label-text">Photo Url</span>
+                        </label>
+                        <input type="text" placeholder="photo url" className="input input-bordered" name="photo" required />
+                    </div>
+                    <div className="form-control mt-6 lg:col-span-2">
+                        <button className="btn bg-blue-100 hover:bg-red-300 text-blue-900 font-bold ">Register</button>
+                    </div>
+                </form>
+                {/* <p className="text-green-600 text-center mb-5"></p>
+                <p className="text-red-600 text-center mb-5"></p> */}
+                <p className="pb-5 text-center">If you  have an account please <Link to='/login' className="text-[#fff]">Log In</Link></p>
+            </div>
         </div>
+    </div>
     );
 };
 
