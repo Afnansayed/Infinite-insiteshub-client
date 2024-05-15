ec18cccd44ac55ef0e4232fa67eeb9124ba51558
Afnansayed <afnansayed1973@gmail.com>
2024-05-15 20:42:34 +0600

    farmer motion use in wish list page

--- src/Pages/WishList/Wish.jsx
+++ src/Pages/WishList/Wish.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import PropType from 'prop-types'
 import axios from "axios";
-
+import {motion} from 'framer-motion'
 const Wish = ({ wish }) => {
     const { _id, photo, title, shortDescription, category, createAt } = wish;
 
@@ -15,27 +15,59 @@ const Wish = ({ wish }) => {
             })
     }
     return (
-        <div className="max-w-lg mx-auto p-4 shadow-md dark:bg-gray-50 dark:text-gray-800">
+        <motion.div className="max-w-lg mx-auto p-4 shadow-md dark:bg-gray-50 dark:text-gray-800"
+        initial={{opacity:0, x:200}}
+        animate={{opacity:1, x:0}}
+        transition={{duration: 1}}
+        >
             <div className="space-y-4">
                 <div className="space-y-2">
-                    <img src={photo} alt="" className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500" />
+                    <motion.img src={photo} alt="" className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"
+                       initial={{opacity:0, x:100}}
+                       animate={{opacity:1,x:0}}
+                       transition={{duration: 1, delay: 0.5}}
+                    />
                     <div className="flex items-center text-xs">
-                        <span>{createAt}</span>
+                        <motion.span
+                        initial={{opacity:0, x:100}}
+                        animate={{opacity:1,x:0}}
+                        transition={{duration: 1, delay: 1}}
+                        >{createAt}</motion.span>
                     </div>
                 </div>
                 <div className="space-y-2">
                     <a rel="noopener noreferrer" href="#" className="block">
-                        <h3 className="text-xl font-semibold dark:text-violet-600">{title}</h3>
+                        <motion.h3 className="text-xl font-semibold dark:text-violet-600"
+                        initial={{opacity:0, x:100}}
+                        animate={{opacity:1,x:0}}
+                        transition={{duration: 1, delay: 1.5}}
+                        >{title}</motion.h3>
                     </a>
-                    <p className="text-xl font-bold text-green-500">{category}</p>
-                    <p className="leading-snug dark:text-gray-600">{shortDescription}</p>
+                    <motion.p className="text-xl font-bold text-green-500"
+                    initial={{opacity:0, x:100}}
+                    animate={{opacity:1,x:0}}
+                    transition={{duration: 1, delay: 2}}
+                    >{category}</motion.p>
+                    <motion.p className="leading-snug dark:text-gray-600"
+                    initial={{opacity:0, x:100}}
+                    animate={{opacity:1,x:0}}
+                    transition={{duration: 1, delay: 2.5}}
+                    >{shortDescription}</motion.p>
                 </div>
                 <div className="flex justify-around ">
-                    <Link><button type="button" className="px-8 py-3 font-semibold rounded dark:bg-gray-800 dark:text-gray-100 hover:bg-gray-100 hover:text-blue-400">Details</button></Link>
-                    <button onClick={() => handleDelete(_id)} type="button" className="px-8 py-3 font-semibold rounded dark:bg-gray-800 dark:text-gray-100 hover:bg-gray-100 hover:text-blue-400">Delete</button>
+                    <Link><motion.button type="button" className="px-8 py-3 font-semibold rounded dark:bg-gray-800 dark:text-gray-100 hover:bg-gray-100 hover:text-blue-400"
+                    initial={{opacity:0, x:100}}
+                    animate={{opacity:1,x:0}}
+                    transition={{duration: 1, delay: 3}}
+                    >Details</motion.button></Link>
+                    <motion.button onClick={() => handleDelete(_id)} type="button" className="px-8 py-3 font-semibold rounded dark:bg-gray-800 dark:text-gray-100 hover:bg-gray-100 hover:text-blue-400"
+                    initial={{opacity:0, x:100}}
+                    animate={{opacity:1,x:0}}
+                    transition={{duration: 1, delay: 3.5}}
+                    >Delete</motion.button>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 };
 Wish.propTypes = {
